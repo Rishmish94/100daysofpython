b091517df1d444911f1c91b5980dae011bf9ddca
Rishabh Mishra <info.rishabhmishra@gmail.com>
2024-06-19 12:46:55 +0530

    Day1 completed

---
+++ Day_1/input.py
@@ -0,0 +1,7 @@
+# Using input() to receive user input
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+# Combining input() and print() for interactive messages
+age = input("Enter your age: ")
+print("You are " + age + " years old.")
---
+++ Day_1/var_name.py
@@ -0,0 +1,6 @@
+# Example of good variable naming and commenting
+# Calculate the area of a rectangle
+length = 5
+width = 3
+area = length * width
+print("The area of the rectangle is:", area)  # Output: The area of the rectangle is: 15
